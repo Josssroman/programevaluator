7e1ac85931c8fd9343b9aefc9e792d8ae6f0aae3
Jose Roman Garcia Mtz <Pp_Roman@MacBook-Pro-de-Jose.local>
2017-10-12 15:37:51 -0500

    This programs has been modified in order to find all the c files and compile them

--- prueba.sh
+++ prueba.sh
@@ -6,8 +6,28 @@ find . -name '*.c' -execdir gcc {} -o {}.exe \;
 
 
 # Here the executable is processed by the script p1.in
-for line in $(sudo find -name '*.c.exe');
+for line in $(sudo find . -name "*1.c.exe");
 do
-	echo "Passing the input data to the executable"
-	./$line.c.exe < p1.in
-done;
+	echo "Passing the input  data p1 to the executable"
+	$line  <  p1.in > $line.out
+done
+
+for line2 in $(sudo find . -name "*2.c.exe");
+do
+	echo "Passing the input data p2 to the executable"
+	$line2 < p2.in > $line2.out 
+done
+
+for line3 in $(sudo find . -name "*3.c.exe");
+do 
+	echo "Passing the input data p3 to tha executable"
+	$line3 < p3.in > $line3.out
+done
+
+for line4 in $(sudo find . -name "*4.c.exe");
+do
+	echo "Passing the input data p4 to the executable"
+	$line4 < p4.in > $line4.out
+done
+
+
