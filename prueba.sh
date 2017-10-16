bc59f69c40a59493755eedce8b61f32228cd6326
Jose Roman Garcia Mtz <Pp_Roman@MacBook-Pro-de-Jose.local>
2017-10-15 22:17:50 -0500

    The comparison was added

--- prueba.sh
+++ prueba.sh
@@ -10,20 +10,23 @@ for line in $(sudo find . -name "*1.c.exe");
 do
 	echo "Passing the input  data p1 to the executable"
 	$line  <  p1.in > $line.out
+	#diff $line.out p1R.pr
+	coounter = $(grep -c 10609 p1R.pr)	
 done
-
+# p2.in
 for line2 in $(sudo find . -name "*2.c.exe");
 do
 	echo "Passing the input data p2 to the executable"
-	$line2 < p2.in > $line2.out 
+	$line2 < p2.in > $line2.out
+       #diff $line2.out p1R.pr	
 done
-
+#p3.in
 for line3 in $(sudo find . -name "*3.c.exe");
 do 
 	echo "Passing the input data p3 to tha executable"
 	$line3 < p3.in > $line3.out
 done
-
+#p4.in
 for line4 in $(sudo find . -name "*4.c.exe");
 do
 	echo "Passing the input data p4 to the executable"
